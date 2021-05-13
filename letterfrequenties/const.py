d5935468dbd4028402370232974e142731fcc8d3
Maria Dukmak <62339103+MariaDukmak@users.noreply.github.com>
2021-05-13 16:36:02 +0200

    Added requirements.txt

---
+++ letterfrequenties/const.py
@@ -0,0 +1,9 @@
+"""Defines the constants for this project."""
+from pathlib import Path
+
+ACCEPTED_CHARACTERS = 'abcdefghijklmnopqrstuwxyz '
+SPACE_ALIAS = '_'
+SPECIAL_CHARACTER_ALIAS = '!'
+CHARACTER_SPLITTER = '-'
+
+MATRIX_PATH = Path('trained_matrix')
