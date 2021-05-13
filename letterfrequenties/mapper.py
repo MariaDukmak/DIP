14d34681067460d654978dde9ca61b344b27ba41
Maria Dukmak <62339103+MariaDukmak@users.noreply.github.com>
2021-05-13 13:04:47 +0200

    Made mapper prettier! Using lambda and map

--- letterfrequenties/mapper.py
+++ letterfrequenties/mapper.py
@@ -1,22 +1,24 @@
 """mapper."""
 import sys
 
+accepted_characters = set('abcdefghijklmnopqrstuwxyz ')
+
 
 def read_data(file_name):
     for line in file_name:
         yield line.strip()
 
 
+def character_transform(character):
+    return '!' if character not in accepted_characters else character.replace(' ', '_')
+
+
 def main():
-    letters = set('abcdefghijklmnopqrstuwxyz ')
     data = read_data(sys.stdin)
     for words in data:
         for index, word in enumerate(words[:-1].lower()):
-            l1 = '_' if words[index] not in letters else words[index]
-            l2 = '_' if words[index+1] not in letters else words[index+1]
-            l1.replace(' ', '#')
-            l2.replace(' ', '#')
-            sys.stdout.write(f'{l1}-{l2}\t{1}\n')
+            c1, c2 = map(character_transform, words[index:index+2])
+            sys.stdout.write(f'{c1}-{c2}\t{1}\n')
 
 
 if __name__ == "__main__":
