a4be794430481463c1478f7b50ac3bee9ac5c781
Maria Dukmak <62339103+MariaDukmak@users.noreply.github.com>
2021-05-19 16:38:18 +0200

    Setup project

---
+++ paxos-implementatie/paxos/computer.py
@@ -0,0 +1,12 @@
+class Computer:
+
+    def __init__(self, computer_id, computer_state):
+        self.id = computer_id
+        self.state = computer_state
+        self.failed = False
+
+        # :/
+        # NO
+
+    def deliver_message(self, message):
+        pass
---
+++ paxos-implementatie/paxos/simulation.py
@@ -0,0 +1,7 @@
+
+
+def simulate():
+    pass
+
+# nP , nA, tmax
+# input = 1, 3, 15
