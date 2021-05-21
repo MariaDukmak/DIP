a263674bd0475bd340a70ce2943ce0cf356bd38f
Maria Dukmak <62339103+MariaDukmak@users.noreply.github.com>
2021-05-21 23:40:40 +0200

    Added ugly print statements.

--- paxos_implementatie/paxos/messages.py
+++ paxos_implementatie/paxos/messages.py
@@ -24,38 +24,76 @@ class Message(metaclass=abc.ABCMeta):
         self.destination = dst
         self.value = value
 
+    def __str__(self):
+        return f'{self.source} -> {self.destination} '
+
 
 class Propose(Message):
+    name = "PROPOSE"
+
     def __init__(self, message_destination, message_value):
         super().__init__(None, None, message_destination, message_value)
 
+    def __str__(self):
+        return super(Propose, self).__str__() + f' {self.name} v={self.value}'
+
 
 class Prepare(Message):
+    name = "PREPARE"
+
     def __init__(self, message_id, message_source, message_destination, message_value):
         super().__init__(message_id, message_source, message_destination, message_value)
+
+    def __str__(self):
+        return super(Prepare, self).__str__() + f' {self.name} n= {Computer.id}'
 
 
 class Promise(Message):
+    name = "PROMISE"
+
     def __init__(self, message_id, message_source, message_destination, message_value):
         super().__init__(message_id, message_source, message_destination, message_value)
+
+    def __str__(self):
+        return super(Promise, self).__str__() + f' {self.name} n= {Computer.id} Prior:({None})'
 
 
 class Accept(Message):
+    name = "ACCEPT"
+
     def __init__(self, message_id, message_source, message_destination, message_value):
         super().__init__(message_id, message_source, message_destination, message_value)
+
+    def __str__(self):
+        return super(Accept, self).__str__() + f' {self.name}'
 
 
 class Accepted(Message):
+    name = "ACCEPTED"
+
     def __init__(self, message_id, message_source, message_destination, message_value):
         super().__init__(message_id, message_source, message_destination, message_value)
+
+    def __str__(self):
+        return super(Accepted, self).__str__() + f' {self.name}'
 
 
 class Rejected(Message):
+    name = "REJECTED"
+
     def __init__(self, message_id, message_source, message_destination, message_value):
         super().__init__(message_id, message_source, message_destination, message_value)
+
+    def __str__(self):
+        return super(Rejected, self).__str__() + f' {self.name}'
 
 
 # voor learner nodig
 class Success(Message):
+    name = "SUCCESS"
+
     def __init__(self, message_id, message_source, message_destination, message_value):
         super().__init__(message_id, message_source, message_destination, message_value)
+
+    def __str__(self):
+        return super(Success, self).__str__() + f' {self.name}'
--- paxos_implementatie/paxos/simulation.py
+++ paxos_implementatie/paxos/simulation.py
@@ -1,5 +1,5 @@
 from typing import List, Optional
-from computer import Acceptor, Proposer
+from computer import Acceptor, Proposer, Computer
 from network import Network
 from event import Event
 import messages
@@ -18,6 +18,7 @@ def simulate(tmax: int, events: List[Event]):
     for t in range(tmax):
         if len(network) == 0 or len(events) == 0:
             # The simulation ends when there are no messages or events
+            print(f'{t:03}:')
             return
 
         event = get_event_on_tick(events, t)
@@ -25,14 +26,31 @@ def simulate(tmax: int, events: List[Event]):
             # There is no event on this tick, proceed normally
             message = network.extract_massage()
             network.deliver_message(message)
+            print(f'{t:03}:{message.source} -> {message.destination} {message.name} n={Acceptor.update_greatest_msg_id(message.id)}')
         else:
             # Ignore the queue this tick, the event is more important
             tick, fails, repairs, message_destination, message_value = event
             for computer in fails:
                 computer.failed = True
+                print(f'** {computer.id} kapot **')
+
             for computer in repairs:
                 computer.failed = False
+                print(f'** {computer.id} gerepareerd **')
+
             if message_destination is not None and message_value is not None:
                 message = messages.Propose(message_destination, message_value)
                 network.deliver_message(message)
+                print(f'{t:03}:    -> {message.destination} {message_value}')
+    if network.__len__() == 0:
+        print(f'\n P{Proposer.id} heeft wel consensus (voorgesteld: %voorgesteld%, geaccepteerd: %geaccepteerd%)')
+    else:
+        print(f' \n P{Proposer.id} heeft geen consensus.')
 
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
