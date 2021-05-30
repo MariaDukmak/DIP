5de811396b948f5af119115c0ec7ac5c7021aeee
Maria Dukmak <62339103+MariaDukmak@users.noreply.github.com>
2021-05-31 01:39:37 +0200

    Computer update

--- paxos_implementatie/paxos/computer.py
+++ paxos_implementatie/paxos/computer.py
@@ -1,6 +1,7 @@
-import numpy as np
-import os
 import glob
+import os
+
+import numpy as np
 
 from paxos_implementatie.paxos.messages import *
 from paxos_implementatie.paxos.network import Network
@@ -18,14 +19,14 @@ class Computer(metaclass=abc.ABCMeta):
     def receive_message(self, message: Message) -> Exception:
         """
         Receive a message from another machine in the network
-        :param message:
-        :return:
+        :param message:a message in the netwerk queue
+        :return Exception
         """
         raise NotImplementedError("This function does only work for the subclasses Acceptor and Proposer!")
 
     def __str__(self) -> str:
         """
-        Prints the id of the computer, we begin at 1.
+        Prints the id of the computer, we begin at 1
         """
         return str(self.id + 1)
 
@@ -57,7 +58,7 @@ class Acceptor(Computer):
             # Accept -> Accepted + update prior
             elif isinstance(incoming_m, Accept):
                 self.prior_n, self.prior_v = incoming_m.id.n, incoming_m.value
-                self.network.queue_message(Accepted(incoming_m.id, self,  incoming_m.source, incoming_m.value))
+                self.network.queue_message(Accepted(incoming_m.id, self, incoming_m.source, incoming_m.value))
 
         else:
             # Reject propose if id is less than the previous one
@@ -69,7 +70,6 @@ class Acceptor(Computer):
     def update_greatest_msg_id(self, message_id: MessageId) -> None:
         """
         Update the greatest message id locally using MessageId class
-
         :param message_id: the id of the message
         """
         if message_id > self.greatest_msg_id:
@@ -135,7 +135,7 @@ class Proposer(Computer):
                     self.network.queue_message(Accept(incoming_m.id, self, a, self.accepted_value))
                 self.promises = 0
 
-        # Accepted ->S
+        # Accepted -> Success
         elif isinstance(incoming_m, Accepted):
             self.promises += 1
             if self.majority_message():
@@ -149,6 +149,8 @@ class Proposer(Computer):
                 self.working_id = message_id
                 for a in self.network.acceptors:
                     self.network.queue_message(Prepare(message_id, self, a, incoming_m.value))
+
+        # Proposers back to sleep
         self.sleep = True
 
     def __str__(self) -> str:
@@ -163,28 +165,45 @@ class Learner(Computer):
         self.learned_n = 0
 
     def receive_message(self, incoming_m: Message) -> None:
+        """
+        Receives messages from the netwerk, based on that it reply with a new message
+        :param incoming_m: a message in the netwerk queue
+        """
+        # Success -> Predicted
         if isinstance(incoming_m, Success):
             self.create_matrix(incoming_m.value)
             for a in self.network.acceptors:
                 a.prior_v, a.prior_n = None, None
             self.network.queue_message(Predicted(self, self.learned_n))
-
+        # else is messages not implemented voor learner
         else:
             raise AttributeError("This type message doesn't exist for Learner")
 
     def create_matrix(self, value: str) -> None:
+        """
+        Creat a matrix of the characters, update the matrix with
+        the learned letters and save it.
+        :param value: the letter combination as nl:aa or en:ga
+        """
+        # the accepted charcters
         characters = 'abcdefghijklmnopqrstuwxyz '
+
+        # split the language and the letter combination
         language, *letter_combi = value.split(':')
         letter_combi = [l if l in characters else '!' for l in ':'.join(letter_combi)]
         file_path = os.path.join('learned_matrices', language + '.np')
 
+        # open or create the matrix
         if file_path in glob.glob('learned_matrices/*.np'):
             matrix = np.loadtxt(file_path)
         else:
             matrix = np.zeros([len(characters)] * 2)
 
+        # update the matrix
         matrix[characters.index(letter_combi[0]), characters.index(letter_combi[1])] += 1
         np.savetxt(file_path, matrix)
+
+        # update the local learning counter
         self.learned_n += 1
 
     def __str__(self) -> str:
