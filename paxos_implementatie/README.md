8c3694c5f6c2981f142e00287d5ada1108810693
Maria Dukmak <62339103+MariaDukmak@users.noreply.github.com>
2021-05-31 01:52:50 +0200

    README.md

--- paxos_implementatie/README.md
+++ paxos_implementatie/README.md
@@ -1,19 +1,18 @@
 # Opdracht 3: Paxos-implementatie
 In deze opdracht moeten wij het Paxos algoritme implementeren. Ook voegen we later een uitbreiding[Fase 2] toe waarin we een deel van de vorige opdracht toevoegen aan het algoritme.
 
+## Fase 1
+In fase 1 van de opdracht hebben we het paxos algoritme geïmplementeerd. Om te laten zien dat onze code werkt hebben we 2 voorbeelden van canvas gehaald.  
+Als je het wilt testen/reproduceren kan je de unit-tests in `canvas_examples.py` uitvoeren.
 
-## Runnen
+## Fase 2
+In fase 2 hebben we het algoritme uitgebreid met een Learner. Hiermee hebben we een gedeelte van de Letterfrequenties-opdracht toe kunnen voegen. Hieruit zijn een paar matrixen gekomen.
 
-### Voorbeeld #1
-```
-run_simulation("1 3 0 15\n0 PROPOSE 1 42\n0 END")
-```
-### Voorbeeld #2
-```
-run_simulation("2 3 0 50\n0 PROPOSE 1 42\n8 FAIL PROPOSER 1\n11 PROPOSE 2 37\n26 RECOVER PROPOSER 1\n0 END")
-```    
-### Voorbeeld #3
-```
-run_simulation("1 3 1 10000\n0 PROPOSE 1 nl: g\n100 PROPOSE 1 nl:ga\n200 PROPOSE 1 nl:af\n300 PROPOSE 1 nl:aa\n400 PROPOSE 1 nl:f "
-"\n500 PROPOSE 1 en: g\n600 PROPOSE 1 en:gr\n700 PROPOSE 1 en:re\n800 PROPOSE 1 en:ea"
-"\n900 PROPOSE 1 en:at\n1000 PROPOSE 1 en:t \n0 END"&#41;)```
+In deze matrices is te zien dat elke lettercombinatie succesvol door het algoritme is gekomen:
+
+![English heatmap](paxos/images/en-matrix.png)
+
+![Dutch heatmap](paxos/images/nl-matrix.png)
+
+We kwamen er achter dat de voorbeeld uitkomsten op canvas niet zo nauwkeurig zijn en bij fase 2 was er al helemaal geen, dus onze unittest aanpak werd daardoor wel moeilijker.
+Het zou dus kunnen dat de unittests net anders zijn dan bedoeld. Maar het zijn wel onze inschattingen van wat het zou moeten zijn.
--- paxos_implementatie/tests/canvas_examples.py
+++ paxos_implementatie/tests/canvas_examples.py
@@ -4,7 +4,10 @@ from paxos_implementatie.paxos import simulation
 
 class CanvasExamples(TestCase):
     def test_example1(self):
-        """Example 1 from the canvas page."""
+        """
+        Example 1 from the canvas page.
+        """
+
         simulation_input = "1 3 0 15\n" \
                            "0 PROPOSE 1 42\n" \
                            "0 END"
@@ -28,57 +31,60 @@ class CanvasExamples(TestCase):
         self.assertEqual(expected_output, simulation_output)
 
     def test_example2(self):
-        """Example 2 from the canvas page."""
+        """
+        Example 2 from the canvas page.
+        """
+
         simulation_input = "2 3 0 50\n" \
                            "0 PROPOSE 1 42\n" \
                            "8 FAIL PROPOSER 1\n" \
                            "11 PROPOSE 2 37\n" \
                            "26 RECOVER PROPOSER 1\n" \
                            "0 END\n"
-        expected_output = "000:\t-> P1 PROPOSE v=42\n" \
-                          "001: P1 -> A1 PREPARE n=1\n" \
-                          "002: P1 -> A2 PREPARE n=1\n" \
-                          "003: P1 -> A3 PREPARE n=1\n" \
-                          "004: A1 -> P1 PROMISE n=1 Prior:(n=None, v=None)\n" \
-                          "005: A2 -> P1 PROMISE n=1 Prior:(n=None, v=None)\n" \
-                          "006: A3 -> P1 PROMISE n=1 Prior:(n=None, v=None)\n" \
-                          "007: P1 -> A1 ACCEPT n=1 v=42\n" \
-                          "008: ** P1 kapot **\n" \
-                          "009:\n" \
-                          "010:\n" \
-                          "011:\t-> P2 PROPOSE v=37\n" \
-                          "012: P2 -> A1 PREPARE n=2\n" \
-                          "013: P2 -> A2 PREPARE n=2\n" \
-                          "014: P2 -> A3 PREPARE n=2\n" \
-                          "015: A1 -> P2 PROMISE n=2 Prior:(n=1, v=42)\n" \
-                          "016: A2 -> P2 PROMISE n=2 Prior:(n=None, v=None)\n" \
-                          "017: A3 -> P2 PROMISE n=2 Prior:(n=None, v=None)\n" \
-                          "018: P2 -> A1 ACCEPT n=2 v=42\n" \
-                          "019: P2 -> A2 ACCEPT n=2 v=42\n" \
-                          "020: P2 -> A3 ACCEPT n=2 v=42\n" \
-                          "021: A1 -> P2 ACCEPTED n=2 v=42\n" \
-                          "022: A2 -> P2 ACCEPTED n=2 v=42\n" \
-                          "023: A3 -> P2 ACCEPTED n=2 v=42\n" \
-                          "024:\n" \
-                          "025:\n" \
-                          "026: ** P1 gerepareerd **\n" \
-                          "027: P1 -> A2 ACCEPT n=1 v=42\n" \
-                          "028: P1 -> A3 ACCEPT n=1 v=42\n" \
-                          "029: A1 -> P1 ACCEPTED n=1 v=42\n" \
-                          "030: A2 -> P1 REJECTED n=1\n" \
-                          "031: A3 -> P1 REJECTED n=1\n" \
-                          "032: P1 -> A1 PREPARE n=3\n" \
-                          "033: P1 -> A2 PREPARE n=3\n" \
-                          "034: P1 -> A3 PREPARE n=3\n" \
-                          "035: A1 -> P1 PROMISE n=3 Prior:(n=2, v=42)\n" \
-                          "036: A2 -> P1 PROMISE n=3 Prior:(n=2, v=42)\n" \
-                          "037: A3 -> P1 PROMISE n=3 Prior:(n=2, v=42)\n" \
-                          "038: P1 -> A1 ACCEPT n=3 v=42\n" \
-                          "039: P1 -> A2 ACCEPT n=3 v=42\n" \
-                          "040: P1 -> A3 ACCEPT n=3 v=42\n" \
-                          "041: A1 -> P1 ACCEPTED n=3 v=42\n" \
-                          "042: A2 -> P1 ACCEPTED n=3 v=42\n" \
-                          "043: A3 -> P1 ACCEPTED n=3 v=42\n" \
+        expected_output = "00000:\t-> P1 PROPOSE v=42\n" \
+                          "00001: P1 -> A1 PREPARE n=1\n" \
+                          "00002: P1 -> A2 PREPARE n=1\n" \
+                          "00003: P1 -> A3 PREPARE n=1\n" \
+                          "00004: A1 -> P1 PROMISE n=1 Prior:(n=None, v=None)\n" \
+                          "00005: A2 -> P1 PROMISE n=1 Prior:(n=None, v=None)\n" \
+                          "00006: A3 -> P1 PROMISE n=1 Prior:(n=None, v=None)\n" \
+                          "00007: P1 -> A1 ACCEPT n=1 v=42\n" \
+                          "00008: ** P1 kapot **\n" \
+                          "00009:\n" \
+                          "00010:\n" \
+                          "00011:\t-> P2 PROPOSE v=37\n" \
+                          "00012: P2 -> A1 PREPARE n=2\n" \
+                          "00013: P2 -> A2 PREPARE n=2\n" \
+                          "00014: P2 -> A3 PREPARE n=2\n" \
+                          "00015: A1 -> P2 PROMISE n=2 Prior:(n=1, v=42)\n" \
+                          "00016: A2 -> P2 PROMISE n=2 Prior:(n=None, v=None)\n" \
+                          "00017: A3 -> P2 PROMISE n=2 Prior:(n=None, v=None)\n" \
+                          "00018: P2 -> A1 ACCEPT n=2 v=42\n" \
+                          "00019: P2 -> A2 ACCEPT n=2 v=42\n" \
+                          "00020: P2 -> A3 ACCEPT n=2 v=42\n" \
+                          "00021: A1 -> P2 ACCEPTED n=2 v=42\n" \
+                          "00022: A2 -> P2 ACCEPTED n=2 v=42\n" \
+                          "00023: A3 -> P2 ACCEPTED n=2 v=42\n" \
+                          "00024:\n" \
+                          "00025:\n" \
+                          "00026: ** P1 gerepareerd **\n" \
+                          "00027: P1 -> A2 ACCEPT n=1 v=42\n" \
+                          "00028: P1 -> A3 ACCEPT n=1 v=42\n" \
+                          "00029: A1 -> P1 ACCEPTED n=1 v=42\n" \
+                          "00030: A2 -> P1 REJECTED n=1\n" \
+                          "00031: A3 -> P1 REJECTED n=1\n" \
+                          "00032: P1 -> A1 PREPARE n=3\n" \
+                          "00033: P1 -> A2 PREPARE n=3\n" \
+                          "00034: P1 -> A3 PREPARE n=3\n" \
+                          "00035: A1 -> P1 PROMISE n=3 Prior:(n=2, v=42)\n" \
+                          "00036: A2 -> P1 PROMISE n=3 Prior:(n=2, v=42)\n" \
+                          "00037: A3 -> P1 PROMISE n=3 Prior:(n=2, v=42)\n" \
+                          "00038: P1 -> A1 ACCEPT n=3 v=42\n" \
+                          "00039: P1 -> A2 ACCEPT n=3 v=42\n" \
+                          "00040: P1 -> A3 ACCEPT n=3 v=42\n" \
+                          "00041: A1 -> P1 ACCEPTED n=3 v=42\n" \
+                          "00042: A2 -> P1 ACCEPTED n=3 v=42\n" \
+                          "00043: A3 -> P1 ACCEPTED n=3 v=42\n" \
                           "\n" \
                           "P1 heeft wel consensus (voorgesteld: 42, geaccepteerd: 42)\n" \
                           "P2 heeft wel consensus (voorgesteld: 37, geaccepteerd: 42)\n"\
